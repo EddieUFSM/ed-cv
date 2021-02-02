e42ec70a0236979eaa12475ab1a27d3f06749765
eddieUFSM <edrocha@inf.ufsm.br>
2021-02-02 12:57:26 -0300

    base frc deu

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom"
 import Home from './pages/Home'
 import Eng from './pages/Eng'
+import Frc from './pages/Frc'
+import Deu from './pages/Deu'
 
 export default function Routes() {
     return (
@@ -10,6 +12,8 @@ export default function Routes() {
                 {/** Geral */}
                 <Route exact path="/" render={(props) => <Home {...props} />} />
                 <Route exact path="/eng" render={(props) => <Eng {...props} />} />
+                <Route exact path="/Deu" render={(props) => <Deu {...props} />} />
+                <Route exact path="/Frc" render={(props) => <Frc {...props} />} />
             </Switch>
         </BrowserRouter>
     )
